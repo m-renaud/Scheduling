ffca5a6e00d9b92f077d1e1064c42827e89e301c
Matt Renaud <mrenaud92@gmail.com>
2015-07-01 13:15:47 -0700

    Added round robin sample command.

--- Playground.hs
+++ Playground.hs
@@ -8,3 +8,8 @@ import Scheduling.Types
 jobList :: [Job]
 jobList = [mkJob "A" 4 (Priority 0), mkJob "B" 6 (Priority 0), mkJob "C" 7 (Priority 0)]
 
+-- | Run the round robin scheduling algorithm on 'jobList' with 2 cpu second time slices.
+--   The scheduler's decision will be printed at each step and at the end the list of
+--   jobs in the order they completed will be returned.
+roundRobinSample :: IO ((), [String])
+roundRobinSample = runJobs jobList RR.preprocessJobs (RR.scheduleJob 2)
